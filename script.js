ec3d67b3f18d396ecd8470e7659fcfcbb50f0737
FaizahM1 <f_muthana@u.pacific.edu>
2026-07-28 23:42:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -25,22 +25,20 @@ document.getElementById('logoLink').addEventListener('click', e => {
   window.scrollTo({ top: 0, behavior: 'smooth' })
 })
  
-function makeSVG(color) {
+function makeSVG() {
   const petals = [0,60,120,180,240,300].map(a =>
-    `<ellipse rx="7" ry="13" transform="rotate(${a})" fill="${color}" opacity="0.9"/>`
+    `<ellipse rx="7" ry="13" transform="rotate(${a})" fill="#e090a8" opacity="0.9"/>`
   ).join('')
   return 'data:image/svg+xml,' + encodeURIComponent(
-    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><g transform="translate(20,20)">${petals}<circle r="5" fill="#6b1212"/></g></svg>`
+    `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40"><g transform="translate(20,20)">${petals}<circle r="5" fill="#c47090"/></g></svg>`
   )
 }
- 
-const colors = ['#8b2020','#7a1a1a','#9b3030','#a03535']
  
 function drop(src) {
   const img = document.createElement('img')
   img.src = src
   img.style.cssText = 'position:fixed;pointer-events:none;z-index:9999;opacity:0;'
-  const size = 18 + Math.random() * 20
+  const size = 22 + Math.random() * 24
   img.style.left = (Math.random() * window.innerWidth) + 'px'
   img.style.top = (-size - 10) + 'px'
   img.style.width = size + 'px'
@@ -65,17 +63,19 @@ function drop(src) {
   requestAnimationFrame(step)
 }
  
-let pngOk = false
-const test = new Image()
-test.onload = () => { pngOk = true }
-test.src = 'flower1.png'
+// test if png files are there
+let png3ok = false, png4ok = false
+const t3 = new Image(); t3.onload = () => { png3ok = true }; t3.src = 'flowers3.png'
+const t4 = new Image(); t4.onload = () => { png4ok = true }; t4.src = 'flowers4.png'
  
 function startRain(count) {
   for (let i = 0; i < count; i++) {
     setTimeout(() => {
-      const src = pngOk
-        ? (Math.random() > 0.5 ? 'flower1.png' : 'flower2.png')
-        : makeSVG(colors[Math.floor(Math.random() * colors.length)])
+      let src
+      if (png3ok && png4ok) src = Math.random() > 0.5 ? 'flowers3.png' : 'flowers4.png'
+      else if (png3ok) src = 'flowers3.png'
+      else if (png4ok) src = 'flowers4.png'
+      else src = makeSVG()
       drop(src)
     }, i * 70)
   }
